82ff2a70e1ce0f4a5097422cedc2eaaab969455a
hll001 <huangbobojiayou@gmail.com>
2022-04-02 23:00:44 +0800

    修改博客首页，优化配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,21 @@
 module.exports = {
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '笔记',
+      description: '好记性不如烂笔头！'
+    }
+  },
   themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Blogs', link: '/tools/使用nvm管理node' },
+      { text: 'GitHub', link: 'https://github.com/hll001' },
+      { text: '默认主题配置', link: 'https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E9%A6%96%E9%A1%B5' }
+    ],
     sidebar: {
       '/tools/': ['使用nvm管理node', 'nginx那些事', '使用hexo建立本地博客'],
       '/': ['']
     }
-    // sidebar: [
-    //   {
-    //     title: '欢迎学习',
-    //     path: '/',
-    //     collapsable: false, // 不折叠
-    //     children: [{ title: '学前必读', path: '/' }]
-    //   },
-    //   {
-    //     title: '工具',
-    //     path: '/tools',
-    //     collapsable: false, // 不折叠
-    //     children: [
-    //       { title: '使用nvm管理node', path: '/使用nvm管理node' },
-    //       { title: 'nginx那些事', path: '/nginx那些事' }
-    //     ]
-    //   }
-    // ]
   }
 }
